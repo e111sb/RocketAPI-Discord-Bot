4f3b7fde8f6cbddb5c8c3008218a51fbde287bf9
Ellis Bowman <ellis.bowman@icloud.com>
2021-10-28 00:13:24 +0100

    V1.0 Working bot, returns api data correctly, handles errors, allows a regular command, search command and help command.

--- bot.js
+++ bot.js
@@ -15,14 +15,20 @@ function sendEmbeddedMessage(channel, newData) {
     .setFooter("\u2800".repeat(1000)+"|")
     channel.send({embeds : [embed]});
 }
-function getLaunches() {
-    return fetch('http://lldev.thespacedevs.com/2.2.0/launch/upcoming?limit=10')
+function getLaunches(channel, searchQuery) {
+    let URL = "http://ll.thespacedevs.com/2.2.0/launch/upcoming?limit=10";
+    if (searchQuery) {
+        URL = URL + "&search=" + searchQuery;
+    }
+    console.log(URL);
+    fetch(URL)
     .then(response => response.json())
     .then(data => {
-        return parseData(data);
+        sendEmbeddedMessage(channel, parseData(data));
     })
     .catch((error) => {
-        return error;
+        console.log(error);
+        channel.send("Sorry, there was an error, please try again later.")
     });
 }
 function parseData(JSONToParse) {
@@ -31,23 +37,30 @@ function parseData(JSONToParse) {
 
     const newData = JSONToParse.results.map((obj) => {
         const launchDate = new Date(obj.net);
+        const formattedLaunchDate = launchDate.toLocaleString({
+            day : "numeric",
+            month : "long",
+            year : "numeric"
+        });
 
-        let daysToLaunch = (launchDate.getTime() - currentTimeInMS) / (1000 * 60 * 60 * 24);
+        let daysToLaunch = Math.trunc((launchDate.getTime() - currentTimeInMS) / (1000 * 60 * 60 * 24));
         if (daysToLaunch < 0) {
             daysToLaunch = "Launched"
         }
-        else {
-            daysToLaunch = "Launch in: " + Math.trunc(daysToLaunch) + " days";
+        else if (daysToLaunch == 0) {
+            daysToLaunch = "Today"
         }
+        else {
+            daysToLaunch = "Launch in: " + daysToLaunch + " days";
+        }
+
         return {
-        
-        name : obj.rocket.configuration.family + " " + daysToLaunch,
-        value : obj.rocket.configuration.name + "\n"
-         + ""
-           + "\n"
-        + obj.launch_service_provider.name}
+            name : obj.rocket.configuration.family + " " + daysToLaunch,
+            value : obj.rocket.configuration.name + "\n"
+            + formattedLaunchDate + "\n"
+            + obj.launch_service_provider.name
+        }
     })
-        console.log("e");
     return newData;
 }
 
@@ -59,25 +72,21 @@ client.on("messageCreate", (message) => {
   if (message.content.startsWith("!rocket") && !message.author.bot) {
     const splitMessage = message.content.split(" ");
     const splitMessageLength = splitMessage.length;
-    if (splitMessageLength > 3) {
-        message.channel.send("Your message contains too many attributes. Please use just !rocket to get upcoming launches, or !rocket StartDate EndDate for a date range.");
+    if (splitMessageLength > 2) {
+        message.channel.send("Your message contains too many attributes. Use \"!rocket help\" for details.");
     }
     else if (splitMessageLength === 1) {
-        getLaunches(message.channel).then(result => {
-            if (result instanceof Error){
-                console.log(result)
-                message.channel.send("Sorry, there was an error, please try again later.")
-            }
-            else {
-                console.log(result)
-                sendEmbeddedMessage(message.channel, result);
-            }
-        });
-
+        getLaunches(message.channel);
     }
-    else if (splitMessageLength === 3) {
-        console.log("rocket range")
+    else if (splitMessageLength === 2) {
+        if (splitMessage[1] !== "help") {
+            getLaunches(message.channel, splitMessage[1])
+        }
+        else {
+            message.channel.send("Please use just !rocket to get upcoming launches, or !rocket SearchTerm in order to search for specific organisations/rockets.");
+        }
     }
+    
   }
 });
 
